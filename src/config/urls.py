114d83fa6d1953e8493b8f112d884c9403fefd76
Dieudonne Munganga <dieudonneishara@gmail.com>
2025-08-06 17:20:46 +0200

    i18nalization

--- src/config/urls.py
+++ src/config/urls.py
@@ -19,11 +19,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
+from django.views.i18n import set_language
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),  # django-allauth URLs
     path('tinymce/', include('tinymce.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),  # Language switching
 ]
 
 # Add i18n URL patterns
